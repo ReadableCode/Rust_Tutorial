2f581e47e8f5c61c65fce3aa9f9196c6386b511a
jason <jasonwchristiansen@gmail.com>
2024-07-10 23:01:41 -0500

    add threading

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{SystemTime, UNIX_EPOCH, Duration};
 use std::fs::{create_dir_all, OpenOptions};
 use std::io::Write;
 use csv::Writer;
 use hostname::get;
+use std::thread;
 
 mod constants;
 mod sum_squares_perf;
 mod introductions;
+
+fn thread_demo() {
+    // Number of threads to create
+    let num_threads = 5;
+
+    // Vector to hold the thread handles
+    let mut handles = vec![];
+
+    for i in 0..num_threads {
+        // Spawn a new thread
+        let handle = thread::spawn(move || {
+            println!("Thread {} is starting.", i);
+            thread::sleep(Duration::from_secs(1));
+            println!("Thread {} is done.", i);
+        });
+
+        // Store the thread handle
+        handles.push(handle);
+    }
+
+    // Wait for all threads to finish
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("All threads are done.");
+}
 
 fn write_to_log_file(content: &str) {
     // Get the data directory from constants
@@ -63,6 +91,12 @@ fn main() {
     // Hostname
     let hostname: String = get().unwrap().into_string().unwrap();
     println!("Hostname: {}", hostname);
+    
+    // Do Thread Demo
+    let do_thread_demo = true;
+    if do_thread_demo {
+        thread_demo();
+    }
 
     // Introductions
     let do_introductions = false;
@@ -71,24 +105,27 @@ fn main() {
     }
 
     // Performance Tests
-    let (rust_time, python_time, go_time) = sum_squares_perf::compare_rust_python_go_performance();
+    let do_performance_tests = false;
+    if do_performance_tests {
+        let (rust_time, python_time, go_time) = sum_squares_perf::compare_rust_python_go_performance();
 
-    // Write results to log file with a timestamp
-    let timestamp: u64 = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_secs();
+        // Write results to log file with a timestamp
+        let timestamp: u64 = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs();
 
-    // Write to log file
-    let result_log_entry = format!(
-        "{}, {}, Rust: {:.2} ms, Python: {:.2} ms, Go: {:.2} ms\n",
-        timestamp, hostname, rust_time, python_time, go_time
-    );
-    write_to_log_file(&result_log_entry);
-    
-    // Write to csv file
-    write_to_csv_file(timestamp, hostname, rust_time, python_time, go_time);
+        // Write to log file
+        let result_log_entry = format!(
+            "{}, {}, Rust: {:.2} ms, Python: {:.2} ms, Go: {:.2} ms\n",
+            timestamp, hostname, rust_time, python_time, go_time
+        );
+        write_to_log_file(&result_log_entry);
+        
+        // Write to csv file
+        write_to_csv_file(timestamp, hostname, rust_time, python_time, go_time);
 
-    // Graph results
-    sum_squares_perf::display_graph(rust_time, python_time, go_time);
+        // Graph results
+        sum_squares_perf::display_graph(rust_time, python_time, go_time);
+    }
 }
